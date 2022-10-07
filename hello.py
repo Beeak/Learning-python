879fb61a77ece59423f9d8a29f2d4a081582349f
beeak <dudetitan@gmail.com>
2022-10-07 11:28:43 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from ast import If
+
+
+username = input("Enter your username: ")
+
+print("Hello, " + username + ".")
+
+a = input("Enter a number for a: ")
+b = input("Enter a number for b: ")
+
+print(int(a) + int(b))
---
+++ yl1.py
@@ -0,0 +1,9 @@
+from decimal import Decimal
+import decimal
+
+
+kroon = decimal(input("Sisesta kroonid: "))
+valem = (kroon * 15 + decimal(.64))
+eur = (kroon * valem)
+
+print("Sinu raha eurodes on " + eur)
