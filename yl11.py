04db09deb071e151537ec43a6e4bf3ba74cbcd48
beeak <dudetitan@gmail.com>
2022-11-14 11:37:08 +0200

    some done and fixed

--- yl11.py
+++ yl11.py
@@ -1,6 +1,12 @@
-import string
+import math
 
-string = str(input("Sisesta string: "))
-values = list(string)
+text = input("Input: ").strip()
 
-if string
+print(len(text))
+
+if len(text) >= 7 and len(text) % 2 == 1:
+    print("The text passes.")
+    i = math.floor(len(text) / 2)
+    print(text(i-1), i, i+1)
+else:
+    print("The text doesn't pass.")
--- yl17.py
+++ yl17.py
@@ -1,2 +1,4 @@
-for x in range(1, 6):
-    print(x)
+i = 1
+while (i <= 5):
+    print(i)
+    i += 1
