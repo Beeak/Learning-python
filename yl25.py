c0cdaa0003cfa0edb82d61038d30b29bd0086401
beeak <dudetitan@gmail.com>
2023-01-19 18:28:50 +0200

    quick fix

--- yl25.py
+++ yl25.py
@@ -19,7 +19,7 @@ if "personal-id" in dictionary:
 else:
     print("personal-id is not in dictionary")
 
-print("There are " + len(dictionary) + " items in the dictionary")
+print("There are " + str(len(dictionary)) + " items in the dictionary")
 
 dictionary = {
     "first-name": "Yes",
@@ -32,7 +32,13 @@ dictionary = {
 
 print(dictionary)
 
-del dictionary["birth-year"]
-dictionary.clear()
+print("-------------------")
 
+del dictionary["birth-year"]
+
+print(dictionary)
+print("The birth-year has been deleted")
+
+print("-------------------")
+dictionary.clear()
 print(dictionary)
