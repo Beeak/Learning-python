b0d364f8b76b33d6a1c185171071c5ea5348eaea
beeak <dudetitan@gmail.com>
2023-01-26 10:19:30 +0200

    26 fix

--- yl26.py
+++ yl26.py
@@ -1,13 +1,82 @@
-input_list = [line.split() for line in input(
-    'Enter the input: ').split('\n') if line]
-input_splice = input_list.index(['Expenses'])
-commissions = dict.fromkeys(input_list[1], 0)
-revenues = {elem[0]: {name: int(revenue) for name, revenue in zip(
-    input_list[1], elem[1:])} for elem in input_list[2: input_splice]}
+sales = {
+    "Johnver": {
+        "revenue": {
+            "tea": 190,
+            "coffee": 325,
+            "water": 682,
+            "milk": 829,
+        },
+        "expenses": {
+            "tea": 120,
+            "coffee": 300,
+            "water": 50,
+            "milk": 67,
+        }
+    },
+    "Vanston": {
+        "revenue": {
+            "tea": 140,
+            "coffee": 19,
+            "water": 14,
+            "milk": 140,
+        },
+        "expenses": {
+            "tea": 65,
+            "coffee": 10,
+            "water": 299,
+            "milk": 254,
+        }
+    },
+    "Danbree": {
+        "revenue": {
+            "tea": 1926,
+            "coffee": 293,
+            "water": 852,
+            "milk": 609,
+        },
+        "expenses": {
+            "tea": 890,
+            "coffee": 23,
+            "water": 1290,
+            "milk": 87,
+        }
+    },
+    "Vansey": {
+        "revenue": {
+            "tea": 14,
+            "coffee": 1491,
+            "water": 56,
+            "milk": 120,
+        },
+        "expenses": {
+            "tea": 54,
+            "coffee": 802,
+            "water": 12,
+            "milk": 129,
+        }
+    },
+    "Mundyke": {
+        "revenue": {
+            "tea": 143,
+            "coffee": 162,
+            "water": 659,
+            "milk": 87,
+        },
+        "expenses": {
+            "tea": 430,
+            "coffee": 235,
+            "water": 145,
+            "milk": 76,
+        }
+    }
+}
 
-for elem in input_list[input_splice + 2:]:
-    for name, expense in zip(input_list[1], elem[1:]):
-        commissions[name] += max(0, revenues[elem[0]][name] - int(expense))
-
-print(' '*12 + ' '.join(f'{key:>{max(len(key), len(str(val)))}}' for key, val in commissions.items()) + '\nCommission  ' +
-      ' '.join(f'{str(round(val * 0.062, 2)):>{max(len(key), len(str(val)))}}' for key, val in commissions.items()))
+for employee_name, employee_sales in sales.items():
+    comission = 0
+    for product, revenue in employee_sales["revenue"].items():
+        profit = revenue - employee_sales["expenses"][product]
+        if profit > 0:
+            profit_calc = profit * 0.062
+            comission += profit * 0.062
+    comission = int(comission)
+    print(employee_name + ": " + str(comission))
